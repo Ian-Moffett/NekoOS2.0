8d2ce61096e7ebd6a3f1b638bc6636b2248cef29
Ian-Moffett <hscorp99906@gmail.com>
2022-03-16 13:26:24 -0400

    Boom! Booted good!

--- kernel/src/kernel.c
+++ kernel/src/kernel.c
@@ -2,6 +2,10 @@
 #include <stddef.h>
 #include "../stivale2.h"
 
+static uint8_t kernel_stack[8000];
+
+
+
 void* get_tag(struct stivale2_struct* stivale2_struct, uint64_t id) {
     struct stivale2_tag* curTag = (void*)stivale2_struct->tags;
 
@@ -16,5 +20,42 @@ void* get_tag(struct stivale2_struct* stivale2_struct, uint64_t id) {
         }
 
         curTag = (void*)curTag->next;
+    }
+}
+
+
+static struct stivale2_header_tag_terminal terminal_tag = {
+    .tag = {
+        .identifier = STIVALE2_HEADER_TAG_TERMINAL_ID,
+        .next = 0
+    },
+
+    .flags = 0
+};
+
+
+static struct stivale2_header_tag_framebuffer lfb_tag = {
+    .tag = {
+        .identifier = STIVALE2_HEADER_TAG_FRAMEBUFFER_ID,
+        .next = (uint64_t)&terminal_tag
+    },
+
+    .framebuffer_width = 0,
+    .framebuffer_height = 0,
+    .framebuffer_bpp = 0
+};
+
+
+__attribute__((section(".stivale2hdr"), used)) static struct stivale2_header stivale_hdr = {
+    .entry_point = 0,
+    .stack = (uintptr_t)(kernel_stack + sizeof(kernel_stack)),
+    .flags = (1 << 1) | (1 << 2) | (1 << 3) | (1 << 4),
+    .tags = (uintptr_t)&lfb_tag
+};
+
+
+void _start(struct stivale2_struct* ss) {
+    while (1) {
+        __asm__ __volatile__("hlt");
     }
 }
